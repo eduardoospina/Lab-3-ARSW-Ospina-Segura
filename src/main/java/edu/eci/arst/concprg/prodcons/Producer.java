2649f3f815b22dd931d25c1a0e17327537c49877
unknown <eduardo.ospina@labinfo.is.escuelaing.edu.co>
2022-02-02 09:32:03 -0500

    parte 1 punto 3

--- src/main/java/edu/eci/arst/concprg/prodcons/Producer.java
+++ src/main/java/edu/eci/arst/concprg/prodcons/Producer.java
@@ -32,9 +32,11 @@ public class Producer extends Thread {
     public void run() {
         while (true) {
 
-            dataSeed = dataSeed + rand.nextInt(100);
-            System.out.println("Producer added " + dataSeed);
-            queue.add(dataSeed);
+            if (queue.size() < stockLimit) {
+                dataSeed = dataSeed + rand.nextInt(100);
+                System.out.println("Producer added " + dataSeed);
+                queue.add(dataSeed);
+            }
             
             try {
                 Thread.sleep(1000);
--- src/main/java/edu/eci/arst/concprg/prodcons/StartProduction.java
+++ src/main/java/edu/eci/arst/concprg/prodcons/StartProduction.java
@@ -20,10 +20,10 @@ public class StartProduction {
     
     public static void main(String[] args) {
         
-        Queue<Integer> queue=new LinkedBlockingQueue<>(5);
+        Queue<Integer> queue=new LinkedBlockingQueue<>(50);
         
         
-        new Producer(queue,Long.MAX_VALUE).start();
+        new Producer(queue,50).start();
         
         //let the producer create products for 5 seconds (stock).
         try {
